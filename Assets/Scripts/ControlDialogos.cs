a0e6572a1fed1f480008ffe2e957f2aa116f287f
Dastdjc <90397187+Dastdjc@users.noreply.github.com>
2021-10-07 16:36:37 +0200

    Principio de scripts

--- Assets/Scripts/ControlDialogos.cs
+++ Assets/Scripts/ControlDialogos.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hola : MonoBehaviour
+public class ControlDialogos : MonoBehaviour
 {
+
+    private Animator anim;
+    private Queue<string> colaDialogos;
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/Texto.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Texto
+{
+    [Text Area(2, 6)]
+    public string[] arrayTexto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
