93d6a04422ab509f26b1de335037eaffe9d63708
Pitarcus <pitarcusmp@gmail.com>
2021-11-02 22:36:45 +0100

    Fixed Timing and Values

--- Assets/Scripts/Musical Bees Minijuego 2/Note.cs
+++ Assets/Scripts/Musical Bees Minijuego 2/Note.cs
@@ -18,17 +18,20 @@ public class Note : MonoBehaviour
     {
         double timeSinceInstantiated = SongManager.GetAudioSourceTime() - timeInstantiated;
 
-        float t = (float)(timeSinceInstantiated / (SongManager.Instance.noteTime * 2));
-
+        float t = (float)(timeSinceInstantiated / (SongManager.Instance.noteTime * 2)); //assignedTime + SongManager.Instance.marginOfError);
 
         if (t > 1)
         {
             Destroy(gameObject);
         }
-        else
+        else if (t > 0.5)
         {
             //transform.localPosition = Vector3.Lerp(Vector3.up * SongManager.Instance.noteSpawnScale, Vector3.up * SongManager.Instance.noteDespawnScale, t);
-            transform.localScale = Vector3.Lerp( new Vector3(1, 1, 0) * SongManager.Instance.noteSpawnScale, new Vector3(1, 1, 0) * SongManager.Instance.noteDespawnScale, t); // unitario * SongManager.Instance.noteSpawnScale, unitario * SongManager.Instance.noteDespawnScale
+            //transform.localScale = Vector3.Lerp(new Vector3(1, 1, 0) * SongManager.Instance.noteSpawnScale, new Vector3(1, 1, 0) * SongManager.Instance.noteDespawnScale, t); // unitario * SongManager.Instance.noteSpawnScale, unitario * SongManager.Instance.
+        }
+        else 
+        {
+            transform.localScale = Vector3.Lerp(new Vector3(1, 1, 0) * SongManager.Instance.noteSpawnScale, new Vector3(1, 1, 0) * SongManager.Instance.noteTapScale, t*2); // unitario * SongManager.Instance.noteSpawnScale, unitario * SongManager.Instance.noteDespawnScale
             GetComponent<SpriteRenderer>().enabled = true;
         }
     }
