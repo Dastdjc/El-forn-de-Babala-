d7ca56af2b4c8cbc32ca06385b8982e8e8bccae5
Pitarcus <pitarcusmp@gmail.com>
2021-11-02 23:39:53 +0100

    Added New Sprites and  sounds

--- Assets/Scripts/Musical Bees Minijuego 2/Note.cs
+++ Assets/Scripts/Musical Bees Minijuego 2/Note.cs
@@ -20,16 +20,11 @@ public class Note : MonoBehaviour
 
         float t = (float)(timeSinceInstantiated / (SongManager.Instance.noteTime * 2)); //assignedTime + SongManager.Instance.marginOfError);
 
-        if (t > 1)
+        if (t > 0.7)
         {
             Destroy(gameObject);
         }
-        else if (t > 0.5)
-        {
-            //transform.localPosition = Vector3.Lerp(Vector3.up * SongManager.Instance.noteSpawnScale, Vector3.up * SongManager.Instance.noteDespawnScale, t);
-            //transform.localScale = Vector3.Lerp(new Vector3(1, 1, 0) * SongManager.Instance.noteSpawnScale, new Vector3(1, 1, 0) * SongManager.Instance.noteDespawnScale, t); // unitario * SongManager.Instance.noteSpawnScale, unitario * SongManager.Instance.
-        }
-        else 
+        else if (t < 0.5)
         {
             transform.localScale = Vector3.Lerp(new Vector3(1, 1, 0) * SongManager.Instance.noteSpawnScale, new Vector3(1, 1, 0) * SongManager.Instance.noteTapScale, t*2); // unitario * SongManager.Instance.noteSpawnScale, unitario * SongManager.Instance.noteDespawnScale
             GetComponent<SpriteRenderer>().enabled = true;
