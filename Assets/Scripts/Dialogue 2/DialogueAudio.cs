78a30d3dca9c6320e0808862162c1fc95026b87f
Pitarcus <pitarcusmp@gmail.com>
2021-11-10 19:27:01 +0100

    Added voices

---
+++ Assets/Scripts/Dialogue 2/DialogueAudio.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+using TMPro;
+
+public class DialogueAudio : MonoBehaviour
+{
+    //private VillagerScript villager;
+    public TMP_Animated animatedText;
+
+    public AudioClip[] voices;
+    public AudioClip[] punctuations;
+    [Space]
+    public AudioSource voiceSource;
+    public AudioSource punctuationSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //villager = GetComponent<VillagerScript>();
+
+
+        animatedText.onTextReveal.AddListener((newChar) => ReproduceSound(newChar));
+    }
+
+    public void ReproduceSound(char c)
+    {
+
+        
+        if (char.IsPunctuation(c) && !punctuationSource.isPlaying)
+        {
+            voiceSource.Stop();
+            punctuationSource.clip = punctuations[Random.Range(0, punctuations.Length)];
+            punctuationSource.Play();
+        }
+
+        if (char.IsLetter(c) && !voiceSource.isPlaying)
+        {
+            punctuationSource.Stop();
+            voiceSource.clip = voices[Random.Range(0, voices.Length)];
+            voiceSource.Play();
+        }
+
+    }
+
+
+
+}
