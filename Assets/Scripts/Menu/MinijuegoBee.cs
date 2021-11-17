edaab75ee111a52164ffbcc28fdf60a5d8afb9a6
Carlos Benedito Tabares <al394807@uji.es>
2021-11-17 19:08:47 +0100

    Oleee

--- Assets/Scripts/Menu/MinijuegoBee.cs
+++ Assets/Scripts/Menu/MinijuegoBee.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MinijuegoBee : MonoBehaviour
 {
-
+    public GameObject task;
     public GameObject Texto;
+
+    private GameObject currentTask;
     private bool DONDESEA;
     public GameObject Player;
     private Rigidbody2D rb;
@@ -20,8 +22,17 @@ public class MinijuegoBee : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E) && DONDESEA == true)
         {
-            SceneManager.LoadScene("Musical Bees", LoadSceneMode.Additive);
-            rb.bodyType = RigidbodyType2D.Static;
+            if(currentTask == null)
+            {
+                currentTask = Instantiate(task, Player.transform);
+                rb.bodyType = RigidbodyType2D.Static;
+            }
+            //SceneManager.LoadScene("Musical Bees", LoadSceneMode.Additive);
+            
+            else
+            {
+                Destroy(currentTask);
+            }
 
 
         }
