e34c2f849ae7d559c90740864c3ee022bcc98a16
HAGSO2 <2agarciasor@gmail.com>
2021-11-24 18:34:13 +0100

    SpritesImport

--- Assets/Scripts/Musical Bees Minijuego 2/BeeSpawner.cs
+++ Assets/Scripts/Musical Bees Minijuego 2/BeeSpawner.cs
@@ -6,26 +6,30 @@ public class BeeSpawner : MonoBehaviour
 {
     public Sprite[] options;
     public GameObject BeeWithoutVisual;
+    private Queue<GameObject> FlyingBees;
     private int pointsGroup = 0;
     // Start is called before the first frame update
     void Start()
     {
         //BeeWithoutVisual.GetComponent<Animation>().Play();
+        FlyingBees = new Queue<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if( ScoreManager.comboScore - pointsGroup > 3)
+        if( ScoreManager.comboScore - pointsGroup > 2)
         {
-            pointsGroup += 3;
-            /*GameObject newBee = Instantiate(BeeWithoutVisual);
-            newBee.SetActive(true);
-            newBee.GetComponent<SpriteRenderer>().sprite = options[Random.Range(0, 4)];
-            newBee.GetComponent<Animation>().Play();*/
-            BeeWithoutVisual.GetComponent<SpriteRenderer>().sprite = options[(int)Random.Range(0, 4)];
-            BeeWithoutVisual.GetComponent<Animation>().Play();
+            pointsGroup += 2;
+            GameObject aux = Instantiate(BeeWithoutVisual);
+            aux.GetComponent<SpriteRenderer>().sprite = options[Random.Range(0, 4)];
+            aux.transform.position = new Vector3(0, 0, 0);
+            aux.GetComponent<Animation>().Play();
+            Debug.Log(aux != null);
+            FlyingBees.Enqueue(aux);
+            
         }
         else if(ScoreManager.comboScore == 0) { pointsGroup = 0; }
+        if (FlyingBees.Count > 0 && FlyingBees.Peek().transform.position.x < -14) { Destroy(FlyingBees.Dequeue()); }
     }
 }
