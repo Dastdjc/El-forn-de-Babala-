a8c91fa2fe781a120f69a207144467e3a5a2fd20
Pitarcus <pitarcusmp@gmail.com>
2021-12-06 18:58:46 +0100

    Drop for Matching berries into Inventory

--- Assets/Scripts/Berry match/eventSystem.cs
+++ Assets/Scripts/Berry match/eventSystem.cs
@@ -17,6 +17,13 @@ public class eventSystem : MonoBehaviour
 
     private float startTime;
 
+    // Ingredietes
+    private int harina;
+    private int levadura;
+    private int aceite;
+    private int mantequilla;
+    private int azucar;
+
     void Start()
     {
         startTime = Time.time;
@@ -71,8 +78,55 @@ public class eventSystem : MonoBehaviour
         Destroy(GameObject.FindWithTag("Verde"));
         Destroy(GameObject.FindWithTag("Rojo"));
 
+        RewardsToInventory();
+
         pantallaFinal.SetActive(true);
         pantallaFinal.GetComponent<Animator>().SetTrigger("aparicion");
         GameObject.Find("MatchingBerries(Clone)/Canvas/PantallaFinalMinijuegoMatchingBerries/Puntuación").GetComponent<TextMeshProUGUI>().text = "Puntuación: " + puntuacion.ToString();
+    }
+
+    private void RewardsToInventory() 
+    {
+        // Calcular recompensa
+        int multiplicador = puntuacion / 450;
+        harina = (int)(multiplicador * 3 * Random.Range(0.8f, 1f));
+        levadura = (int)(multiplicador * 1 * Random.Range(0.8f, 1f));
+        aceite = (int)(multiplicador * 2 * Random.Range(0.8f, 1f));
+        mantequilla = (int)(multiplicador * 1 * Random.Range(0.8f, 1f));
+        azucar = (int)(multiplicador * 3 * Random.Range(0.8f, 1f));
+
+        // Añadir dentro del inventario
+        Inventory inventario = GameObject.Find("INVENTORY/Inventory").GetComponent<Inventory>();
+
+        Items itemHarina = ScriptableObject.CreateInstance<Items>();
+        itemHarina.amount = harina;
+        itemHarina.type = "Harina";
+        if (itemHarina.amount > 0)
+            inventario.AddIngrItem(itemHarina);
+
+        Items itemLevadura = ScriptableObject.CreateInstance<Items>();
+        itemLevadura.amount = levadura;
+        itemLevadura.type = "Levadura";
+        if (itemLevadura.amount > 0)
+            inventario.AddIngrItem(itemLevadura);
+
+        Items itemAceite = ScriptableObject.CreateInstance<Items>();
+        itemAceite.amount = aceite;
+        itemAceite.type = "Aceite";
+        if (itemAceite.amount > 0)
+            inventario.AddIngrItem(itemAceite);
+
+        Items itemMantequilla = ScriptableObject.CreateInstance<Items>();
+        itemMantequilla.amount = mantequilla;
+        itemMantequilla.type = "Mantequilla";
+        if (itemMantequilla.amount > 0)
+            inventario.AddIngrItem(itemMantequilla);
+
+        Items itemAzucar = ScriptableObject.CreateInstance<Items>();
+        itemAzucar.amount = azucar;
+        itemAzucar.type = "Azúcar";
+        if (itemAzucar.amount > 0)
+            inventario.AddIngrItem(itemAzucar);
+
     }
 }
